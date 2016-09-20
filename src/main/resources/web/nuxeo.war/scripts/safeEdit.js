50056f412eb293c5de7ddd1f2d206f4c7aa46cb9
Harlan Brown <hbrown@nuxeo.com>
2016-09-20 16:21:33 -0500

    add .fn.checkSafeEditOnForms

--- src/main/resources/web/nuxeo.war/scripts/safeEdit.js
+++ src/main/resources/web/nuxeo.war/scripts/safeEdit.js
@@ -1,8 +1,11 @@
 (function($) {
 
   $.fn.initSafeEdit = function(key, savePeriod, saveCB, loadCB, message) {
-
     return this;
+  }
+
+  $.fn.checkSafeEditOnForms = function(message) {
+    return false;
   }
 
 })(jQuery);
